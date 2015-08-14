ebb2b1c4dcdca6abd044520c5f9e2d70594f5f2a
Shawn Kumar <shawn.kumar@careerbuilder.com>
2015-08-14 08:56:01 -0400

    Reverted back to regular document.write

--- mxmviewer.js
+++ mxmviewer.js
@@ -5,11 +5,13 @@ $(document).click(function(e) {
     var target = $(e.target);
 
     if(target.text().indexOf("<table") != -1){
-      var blobData = target.text();
-      var file = new Blob([blobData], {encoding:"UTF-8", type: 'text/html;charset=UTF-8'});
-      var fileURL = URL.createObjectURL(file);
+      var mxmWindow = window.open();
+      mxmWindow.document.write(target.text());
+     // var blobData = target.text();
+     // var file = new Blob([blobData], {encoding:"UTF-8", type: 'text/html;charset=UTF-8'});
+     // var fileURL = URL.createObjectURL(file);
 
-      var mxmWindow = window.open(fileURL);
+     // var mxmWindow = window.open(fileURL);
 
       //var mxmWindow = window.open();
       //mxmWindow.document.write(target.text());

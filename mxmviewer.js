a8201818add543fd9765a02bc8bd86ac8c6fb244
Shawn Kumar <shawn.kumar@careerbuilder.com>
2015-08-24 09:52:31 -0400

    Changed this to be a simple output into a new window. Added another matching url

--- mxmviewer.js
+++ mxmviewer.js
@@ -4,23 +4,14 @@ $(document).click(function(e) {
   if (e.shiftKey) {
     var target = $(e.target);
 
-    if(target.text().indexOf("<table") != -1){
+    var targetData = $(target).text();
+    if(looksLikeHtml(targetData)) {
       var mxmWindow = window.open();
-      mxmWindow.document.write(target.text());
-     // var blobData = target.text();
-     // var file = new Blob([blobData], {encoding:"UTF-8", type: 'text/html;charset=UTF-8'});
-     // var fileURL = URL.createObjectURL(file);
-
-     // var mxmWindow = window.open(fileURL);
-
-      //var mxmWindow = window.open();
-      //mxmWindow.document.write(target.text());
-
-     // mxmWindow.document.open();
-     // mxmWindow.document.write(target.text());
-     // mxmWindow.document.close();
-     // $(mxmWindow.document).find('body').hide().show(100);
-      //$(w.document).html(target.text());
+      mxmWindow.document.write(targetData);
     }
   } 
 });
+
+function looksLikeHtml(input){
+  return input.indexOf("<") != -1 && input.indexOf(">") != -1;
+}
